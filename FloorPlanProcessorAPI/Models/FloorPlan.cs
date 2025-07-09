a9683f93e322f61bcee7d975d8e9678b76866f2b
Datn7 <datunashvili72@gmail.com>
2025-07-09 23:27:51 +0400

    added floorplan model and packages

---
+++ FloorPlanProcessorAPI/Models/FloorPlan.cs
@@ -0,0 +1,11 @@
+﻿namespace FloorPlanProcessorAPI.Models
+{
+    public class FloorPlan
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string FileName { get; set; }
+        public string SvgPath { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
